162cfb31fb54f61cf34879bca350c15f51b046da
Alexis Wanja <alexis.wanja@student.moringaschool.com>
2022-01-02 12:44:56 +0300

    add social icons

--- js/script.js
+++ js/script.js
@@ -2,19 +2,27 @@ $('.main').click(function () {
   $(this).children('.hide').slideToggle(1200);
   $(this).children('.show').toggle(1500);
 });
-// giving feedback to the customer
-$("button").click(function (omonge) {
-  var client = document.getElementById('nameDetail').value;
-  alert(' Dear ' + ' esteemed ' + client + ' we have received your message successful. ' + ' Feel free to reach out to us anytime');
-  omonge.preventDefault();
-});
-//    reset my form button
+
+
+$('.submit').click(function () {
+  var Name = $('#mce-FNAME').val();
+  var Email = $('#mce-EMAIL').val();
+  var Message = $('#mce-MESSAGE').val();
+  
+  if ($("Name").val() && $("Email").val() && $("Message").val() ){
+      alert ("Message received. Your feedback is appreciated.");
+  } else {
+    alert('Please make sure you have filled in the form correctly!');
+  }
+})
+
+
 $("button").on('click', function () {
   $('form').each(function () {
     this.reset();
   });
 });
-//portifolio section 
+
 $(".card").hover(function () {
   $(this).children(".card-1").fadeToggle(1000, "linear");
 });
